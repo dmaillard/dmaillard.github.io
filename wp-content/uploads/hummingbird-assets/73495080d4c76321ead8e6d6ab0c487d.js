aab431011e4566bbdfb901242982a7651eecf1ec
5mooth0perator <38310568+5mooth0perator@users.noreply.github.com>
2020-03-16 22:06:52 -0300

    up

---
+++ wp-content/uploads/hummingbird-assets/73495080d4c76321ead8e6d6ab0c487d.js
@@ -0,0 +1,13 @@
+/**handles:ds-cli**/
+(function($){
+    $(function(){
+        $('li#wp-admin-bar-ds-cli a').click(function() {
+            $.post(
+                ds_cli.ajaxurl, {
+                    action : 'ds-cli-submit',
+                    nonce : ds_cli.nonce
+                }
+            );
+        });
+    });
+})(jQuery);
